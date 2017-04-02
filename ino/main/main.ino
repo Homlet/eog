749ebb6f72c3a8271f03844d0b228d80685cd300
Homlet <samlhub@gmail.com>
2017-04-02 09:02:53 +0100

    Remove all references to 'servo' to appease SCOTT STREET.

--- ino/main/main.ino
+++ ino/main/main.ino
@@ -1,34 +1,15 @@
 #include <Servo.h>
 
 #define PIN_EOG A1
-#define PIN_SERVO 9
 
-#define SPEED 2
 #define ALPHA 0.9921875
 
-enum Direction {
-  LEFT,
-  NONE,
-  RIGHT
-};
-
-int offset(Direction dir) {
-  switch(dir){
-    case LEFT: return -SPEED;
-    case RIGHT: return SPEED;
-    default:
-    case NONE: return 0;
-  }
-}
-
-Servo servo;
 float mean = 0;
 float dev = 0;
 int cycle = 0;
-Direction movement = NONE;
 
 void setup() {
-  //servo.attach(PIN_SERVO);
+  // Stub.
 }
 
 void loop() {
@@ -38,15 +19,6 @@ void loop() {
   mean = ALPHA * mean + (1 - ALPHA) * value;
   dev = ALPHA * dev + (1 - ALPHA) * off;
 
-//  // Detect if movement happened.
-//  if (inst_dev > 2 * dev) {
-//    if (value - mean > 1) {
-//      movement = LEFT;
-//    } else {
-//      movement = RIGHT;
-//    }
-//  }
-
   // Output the values every 50 cycles.
   cycle++;
   if (cycle == 50) {
@@ -54,7 +26,6 @@ void loop() {
     Serial.print(":");
     Serial.println(dev);
     cycle = 0;
-//    movement = NONE;
   }
 }
 
